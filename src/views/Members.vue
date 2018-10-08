1e4191e1003c61a8499238bc5f36e0e20675d67e
NavySky <kostsalo134@gmail.com>
2018-10-08 15:08:15 +0300

    initial commit

--- src/views/Members.vue
+++ src/views/Members.vue
@@ -6,96 +6,82 @@
         <div class="bg-img"></div>
         <div class="container is-fluid">
           <div class="columns">
-            <table v-if="token" class="table is-bordered is-narrow is-hoverable table is-fullwidth is-size-6 has-text-centered">
+            <table v-if="token" class="table is-bordered is-narrow is-hoverable is-fullwidth is-size-6 has-text-centered">
               <thead class="has-background-info has-text-white has-text-centered">
-              <tr class="has-background-info has-text-white has-text-centered">
-                <th class="has-text-white has-text-centered">№</th>
-                <th class="has-text-white has-text-centered">Имя</th>
-                <th class="has-text-white has-text-centered">Ник</th>
-                <th class="has-text-white has-text-centered">Класс</th>
-                <th class="has-text-white has-text-centered">Оружие</th>
-                <th class="has-text-white has-text-centered">Триграммы</th>
-                <th class="has-text-white has-text-centered">Ожерелье</th>
-                <th class="has-text-white has-text-centered">Серьга</th>
-                <th class="has-text-white has-text-centered">Кольцо</th>
-                <th class="has-text-white has-text-centered">Пояс</th>
-                <th class="has-text-white has-text-centered">Перчатки</th>
-                <th class="has-text-white has-text-centered">Сфера</th>
-                <th class="has-text-white has-text-centered"><abbr title="Дух Защитник">Дух</abbr></th>
-                <th class="has-text-white has-text-centered">Символ</th>
-                <th class="has-text-white has-text-centered">Душа</th>
-                <th class="has-text-white has-text-centered">Подвеска</th>
-                <th class="has-text-white has-text-centered"><abbr title="Урон в секунду">DPS</abbr></th>
-                <th class="has-text-white has-text-centered">Стихия</th>
-              </tr>
+                <tr class="has-background-info has-text-white has-text-centered">
+                  <template v-for="field in tableFields">
+                    <template v-if="field === 'Дух'">
+                      <th class="has-text-white has-text-centered"><abbr title="Дух Защитник">{{field}}</abbr></th>
+                    </template>
+                    <template v-else-if="field === 'DPS'">
+                      <th class="has-text-white has-text-centered"><abbr title="Урон в секунду">{{field}}</abbr></th>
+                    </template>
+                    <template v-else>
+                      <th class="has-text-white has-text-centered">{{field}}</th>
+                    </template>
+                  </template>
+                </tr>
               </thead>
               <tfoot class="has-background-info has-text-white has-text-centered">
-              <tr class="has-background-info has-text-white has-text-centered">
-                <th class="has-text-white has-text-centered">№</th>
-                <th class="has-text-white has-text-centered">Имя</th>
-                <th class="has-text-white has-text-centered">Ник</th>
-                <th class="has-text-white has-text-centered">Класс</th>
-                <th class="has-text-white has-text-centered">Оружие</th>
-                <th class="has-text-white has-text-centered">Триграммы</th>
-                <th class="has-text-white has-text-centered">Ожерелье</th>
-                <th class="has-text-white has-text-centered">Серьга</th>
-                <th class="has-text-white has-text-centered">Кольцо</th>
-                <th class="has-text-white has-text-centered">Пояс</th>
-                <th class="has-text-white has-text-centered">Перчатки</th>
-                <th class="has-text-white has-text-centered">Сфера</th>
-                <th class="has-text-white has-text-centered"><abbr title="Дух Защитник">Дух</abbr></th>
-                <th class="has-text-white has-text-centered">Символ</th>
-                <th class="has-text-white has-text-centered">Душа</th>
-                <th class="has-text-white has-text-centered">Подвеска</th>
-                <th class="has-text-white has-text-centered"><abbr title="Урон в секунду">DPS</abbr></th>
-                <th class="has-text-white has-text-centered">Стихия</th>
-              </tr>
+                <tr class="has-background-info has-text-white has-text-centered">
+                  <template v-for="field in tableFields">
+                    <template v-if="field === 'Дух'">
+                      <th class="has-text-white has-text-centered"><abbr title="Дух Защитник">{{field}}</abbr></th>
+                    </template>
+                    <template v-else-if="field === 'DPS'">
+                      <th class="has-text-white has-text-centered"><abbr title="Урон в секунду">{{field}}</abbr></th>
+                    </template>
+                    <template v-else>
+                      <th class="has-text-white has-text-centered">{{field}}</th>
+                    </template>
+                  </template>
+                </tr>
               </tfoot>
               <tbody class="has-text-centered">
-              <tr v-for="(key, index) in users">
-                <td class="has-text-centered">{{index}}</td>
-                <td class="has-text-centered">{{key["first_name"]}}
-                  <div v-if="key['trainee']">
-                    <abbr title="Неофит"><i class="fas fa-crown has-text-success"></i></abbr></div>
-                  <div v-else>
-                    <div v-if="key['role'] === 'member'">
-                      <abbr title="Ученик"><i class="fas fa-crown has-text-warning"></i></abbr>
+                <tr v-for="(key, index) in users">
+                  <td class="has-text-centered">{{index}}</td>
+                  <td class="has-text-centered">{{key["first_name"]}}
+                    <div v-if="key['trainee']">
+                      <abbr title="Неофит"><i class="fas fa-crown has-text-success"></i></abbr></div>
+                    <div v-else>
+                      <div v-if="key['role'] === 'member'">
+                        <abbr title="Ученик"><i class="fas fa-crown has-text-warning"></i></abbr>
+                      </div>
+                      <div v-else-if="key['role'] === 'staff'">
+                        <abbr title="Ветеран"><i class="fas fa-crown staff-color"></i></abbr>
+                      </div>
+                      <div v-else><abbr title="Глава"><i class="fas fa-crown admin-color"></i></abbr></div>
                     </div>
-                    <div v-else-if="key['role'] === 'staff'">
-                      <abbr title="Ветеран"><i class="fas fa-crown staff-color"></i></abbr>
+                  </td>
+                  <td class="has-text-centered">{{key["nickname"]}}</td>
+                  <td class="has-text-centered">
+                    <div class="box is-centered">
+                      <figure class="image is-32x32">
+                        <ImageClasses v-bind:class-number="key['class']"></ImageClasses>
+                      </figure>
                     </div>
-                    <div v-else><abbr title="Глава"><i class="fas fa-crown admin-color"></i></abbr></div>
-                  </div>
-                </td>
-                <td class="has-text-centered">{{key["nickname"]}}</td>
-                <td class="has-text-centered">
-                  <div class="box is-centered">
-                    <figure class="image is-32x32">
-                      <ImageClasses v-bind:class-number="key['class']"></ImageClasses>
-                    </figure>
-                  </div>
-                </td>
-                <td class="has-text-centered">{{key["equipment"]["weapon"]}}</td>
-                <td class="has-text-centered">{{key["equipment"]["soul_shields"]}}</td>
-                <td class="has-text-centered">{{key["equipment"]["necklace"]}}</td>
-                <td class="has-text-centered">{{key["equipment"]["earrings"]}}</td>
-                <td class="has-text-centered is-vertical-center">{{key["equipment"]["ring"]}}</td>
-                <td class="has-text-centered">{{key["equipment"]["belt"]}}</td>
-                <td class="has-text-centered">{{key["equipment"]["gloves"]}}</td>
-                <td class="has-text-centered">{{key["equipment"]["sphere"]}}</td>
-                <td class="has-text-centered">{{key["equipment"]["spirit"]}}</td>
-                <td class="has-text-centered">{{key["equipment"]["badge"]}}</td>
-                <td class="has-text-centered">{{key["equipment"]["soul"]}}</td>
-                <td class="has-text-centered">{{key["equipment"]["pendant"]}}</td>
-                <td class="has-text-centered">{{key["equipment"]["dps"]}}</td>
-                <td class="has-text-centered">
-                  <div class="box is-centered">
-                    <figure class="image is-32x32">
-                      <ImageElements v-bind:element-number="key['equipment']['element']"></ImageElements>
-                    </figure>
-                  </div>
-                </td>
-              </tr>
+                  </td>
+                  <td class="has-text-centered">{{key["equipment"]["weapon"]}}</td>
+                  <td class="has-text-centered">{{key["equipment"]["soul_shields"]}}</td>
+                  <td class="has-text-centered">{{key["equipment"]["necklace"]}}</td>
+                  <td class="has-text-centered">{{key["equipment"]["earrings"]}}</td>
+                  <td class="has-text-centered is-vertical-center">{{key["equipment"]["ring"]}}</td>
+                  <td class="has-text-centered">{{key["equipment"]["belt"]}}</td>
+                  <td class="has-text-centered">{{key["equipment"]["gloves"]}}</td>
+                  <td class="has-text-centered">{{key["equipment"]["sphere"]}}</td>
+                  <td class="has-text-centered">{{key["equipment"]["spirit"]}}</td>
+                  <td class="has-text-centered">{{key["equipment"]["badge"]}}</td>
+                  <td class="has-text-centered">{{key["equipment"]["soul"]}}</td>
+                  <td class="has-text-centered">{{key["equipment"]["pendant"]}}</td>
+                  <td class="has-text-centered">{{key["equipment"]["dps"]}}</td>
+                  <td class="has-text-centered">
+                    <div class="box is-centered">
+                      <figure class="image is-32x32">
+                        <ImageElements v-bind:element-number="key['equipment']['element']"></ImageElements>
+                      </figure>
+                    </div>
+                  </td>
+                </tr>
               </tbody>
             </table>
           </div>
@@ -119,7 +105,10 @@ export default {
   components: {Loader, NavbarLogo, ImageClasses, ImageElements},
   data: function () {
     return {
-
+      tableFields: [
+        "№", "Имя", "Ник", "Класс", "Оружие", "Триграммы", "Ожерелье", "Серьга", "Кольцо", "Пояс",
+        "Перчатки", "Сфера", "Дух", "Символ", "Душа", "Подвеска", "DPS", "Стихия"
+      ]
     }
   },
   computed: {
